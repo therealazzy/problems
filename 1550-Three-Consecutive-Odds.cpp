44d82740784793d187964f94a17796486670c856
therealazzy <71785801+therealazzy@users.noreply.github.com>
2024-11-27 02:35:03 +0000

    [1550] [Time Beats: 100.00%] [Memory Beats: 11.76%] - LeetPush

--- 1550-Three-Consecutive-Odds.cpp
+++ 1550-Three-Consecutive-Odds.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
       int count = 0;
+      if(arr.size() < 3) return false;
       for (const int a : arr){
         count = a%2 == 0 ? 0 : count+1;
         if( count == 3){
