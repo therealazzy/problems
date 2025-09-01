118c34b4a72f6b96ee0b0a5cf323626782d45817
therealazzy <71785801+therealazzy@users.noreply.github.com>
2025-09-01 12:10:40 +0100

    [1266] [Time Beats: 100.00%] [Memory Beats: 86.99%] - LeetPush

--- Minimum-Time-Visiting-All-Points.cpp
+++ Minimum-Time-Visiting-All-Points.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+    int minTimeToVisitAllPoints(vector<vector<int>>& p) {
         int answer = 0;
-        for(int i = 1; i < points.size(); ++i){
-            answer += max(abs(points[i][0] - points[i-1][0]), abs(points[i][1]-points[i-1][1]));
+        for(int i = 1; i < p.size(); ++i){
+            answer += max(abs(p[i][0] - p[i-1][0]), abs((p[i][1] - p[i-1][1])));
         }
         return answer;
     }
