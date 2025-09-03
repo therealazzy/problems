a2551cd6efedc5ff45435eddf0b6f43174f66be0
therealazzy <71785801+therealazzy@users.noreply.github.com>
2025-09-03 17:05:31 +0100

    [70] [Time Beats: 100.00%] [Memory Beats: 73.61%] - LeetPush

--- Climbing-Stairs.cpp
+++ Climbing-Stairs.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n < 3) return n;
-        vector<int> steps(n+1,0);
-        steps[0] = 1;
-        steps[1] = 1;
-        steps[2] = 2;
+        if(n <= 3) return n;
+        
+        int prev1 = 2;
+        int prev2 = 1;
+        int cur = 0;
 
         for(int i = 3; i <= n; ++i){
-            steps[i] = steps[i-1] + steps[i-2];
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
         }
-        return steps[n];
+
+        return cur;
     }
 };
