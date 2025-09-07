339f71bd52ef2c4ab6e2bfc80a0b3f5b6b07d4e0
therealazzy <71785801+therealazzy@users.noreply.github.com>
2025-09-07 16:36:33 +0100

    [784] [Time Beats: 100.00%] [Memory Beats: 17.18%] - LeetPush

--- Letter-Case-Permutation.cpp
+++ Letter-Case-Permutation.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    vector<string> res;
-
-    void permutate(string cur, string s, int i){
-        if(i==s.size()){
-            res.push_back(cur);
-            return;
-        }
-
-        if(isdigit(s[i])){
-            cur.push_back(s[i]);
-            permutate(cur,s,i+1);
-        } else{
-            string c1 = cur;
-            c1.push_back(tolower(s[i]));
-            permutate(c1,s,i+1);
-
-            string c2 = cur;
-            c2.push_back(toupper(s[i]));
-            permutate(c2,s,i+1);
-        }
-    }
-
-
     vector<string> letterCasePermutation(string s) {
-        res.clear();
-        permutate("",s,0);
+        vector<string> res={""};
+        for( char c : s){
+            vector<string> perm;
+            if(isalpha(c)){
+                for( string& str : res ){
+                    perm.push_back(str + (char)tolower(c));
+                    perm.push_back(str + (char)toupper(c));
+                }
+            }
+            else{
+                for( string& str : res){
+                perm.push_back(str + c);
+                }
+            }
+            res = perm;
+        }
+        
+
         return res;
-    }
+}
+
 };
