045a1814d2d10408b5a0ad4953eced28c08de864
therealazzy <71785801+therealazzy@users.noreply.github.com>
2025-10-15 12:56:45 +0100

    [268] [Time Beats: 100.00%] [Memory Beats: 13.43%] - LeetPush

--- Missing-Number.cpp
+++ Missing-Number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingNumber(vector<int>& n) {
         int res = n.size();
-        for(int i = 0; i < n.size(); i++){
+        for(int i = 0 ; i < n.size(); ++i){
             res += i - n[i];
         }
         return res;
