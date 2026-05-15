a6b153dc412e3a6fd04d9f255d505fed6df0f8f8
therealazzy <71785801+therealazzy@users.noreply.github.com>
2026-05-15 18:07:17 +0100

    [1] [Time Beats: 75.67%] [Memory Beats: 63.39%] - LeetPush

--- Two-Sum.cpp
+++ Two-Sum.cpp
@@ -1,14 +1,12 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& n, int target) {
-     unordered_map<int, int> seen;
-     for( int i = 0; i < n.size(); ++i){
-        int second = target - n[i];
-        if(seen.count(second)){
-            return{seen[second], i};
-        }
-        seen[n[i]] = i;
-     }
-     return{};
-    }
-};
+1class Solution {
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        unordered_map<int, int> seen;
+5        for(int i = 0 ; i < nums.size(); ++i){
+6            int second = target - nums[i];
+7            if(seen.count(second)) return {seen[second], i};
+8            seen[nums[i]] = i;
+9        }
+10        return {};
+11    }
+12};
