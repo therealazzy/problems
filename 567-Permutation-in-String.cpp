dd2778de0a94628309d61db5d617db4fdf8c9152
therealazzy <71785801+therealazzy@users.noreply.github.com>
2026-08-24 14:53:12 +0100

    [567] [Time Beats: 66.24%] [Memory Beats: 17.37%] - LeetPush

---
+++ 567-Permutation-in-String.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int len1 = s1.length();
+        if(len1 > s2.length()){
+            return false;
+        }
+
+        unordered_map<char, int> count1;
+        count1.reserve(26);
+        unordered_map<char, int> count2;
+        count2.reserve(26);
+
+        for( int i = 0; i < len1; ++i){
+            count1[s1[i]]++;
+            count2[s2[i]]++;
+        }
+
+        if(count1 == count2){
+            return true;
+        }
+
+        for(int i = len1; i < s2.length(); ++i){
+            count2[s2[i]]++;
+            if(count2[s2[i - len1]] == 1){
+                count2.erase(s2[i - len1]);
+            }else {count2[s2[i - len1]]--;}
+
+            if(count1 == count2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
