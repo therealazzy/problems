d5858bcd4f6ff9b9953f01876189d9071d812bb2
therealazzy <71785801+therealazzy@users.noreply.github.com>
2026-08-26 15:44:11 +0100

    [15] [Time Beats: 63.36%] [Memory Beats: 92.42%] - LeetPush

--- 15-3Sum.cpp
+++ 15-3Sum.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        //sort
         sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
         for(int i = 0; i < nums.size(); i++){
-            //check if i is a dupe of previous i
-            if(i > 0 && nums[i] == nums[i-1]){
-                continue;
-            }
-            //init j and k pointers
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
             int j = i + 1;
-            int k = nums.size() -1;
-            //while the pointers are valid
+            int k = nums.size() - 1;
+
             while(j < k){
-                //start calculating the sum and what to do if we find a valid sum
                 int sum = nums[i] + nums[j] + nums[k];
-                if(sum < 0 ) j++;
+                if(sum < 0) j++;
                 else if(sum > 0) k--;
-                //we have found a valid throuple
                 else{
                     res.push_back({nums[i], nums[j], nums[k]});
-                    //move j forward to look further
                     j++;
-                    //make sure j is not a duplicate either AND while j is still less than k
-                    while(nums[j] == nums[j - 1] && j < k) j++;
+                    while(nums[j] == nums[j-1] && j<k){
+                        j++;
+                    }
                 }
-
             }
         }
         return res;
